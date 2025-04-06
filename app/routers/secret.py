1795d4e10455cefd9ac67bdbea87ba1c73fcbeb9
EVA666999 <ukratitelkisok9913@inbox.ru>
2025-04-06 05:00:52 +0300

    dasda

--- app/routers/secret.py
+++ app/routers/secret.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+import uuid
 from fastapi import APIRouter, Depends, status, HTTPException, Request
 from sqlalchemy.orm import Session
 from typing import Annotated
@@ -24,6 +25,8 @@ async def create_secret(
     create_secret: CreateSecret,
     _: Annotated[None, Depends(no_cache_headers)]
 ):  
+    
+    secret_key = str(uuid.uuid4())
     encrypted_secret = EncryptionService.encrypt(create_secret.secret)
     
     encrypted_passphrase = None
@@ -42,6 +45,7 @@ async def create_secret(
     result = await db.execute(
         insert(Secret).values(**insert_data).returning(Secret)
     )
+    secret_key_uuid = await db.execute(insert(Secret).values(secret_key).returning(Secret))
     secret = result.scalar_one()
 
     client_ip = request.client.host if request.client else None
@@ -65,10 +69,46 @@ async def create_secret(
         "created_at": secret.created_at.isoformat(),
         "expires_at": secret.expires_at.isoformat() if secret.expires_at else None,
         "ttl_seconds": secret.ttl_seconds,
+
     }
     
     await RedisService.cache_secret(str(secret.id), secret_data, secret.ttl_seconds)
 
     return {
-        "secret_key": str(secret)
+        "secret_key": secret_key
+    }
+
+@router.get('/{secret_key}', status_code=status.HTTP_200_OK)
+async def get_secret(
+                request: Request,
+                _: Annotated[None, Depends(no_cache_headers)],
+                db: Annotated[AsyncSession, Depends(get_db)], 
+                secret_id: int
+            ):
+    secret = await db.scalar(select(Secret).where(Secret.id == secret_id))
+    if secret is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='There is no secret found'
+        )
+    
+    client_ip = request.client.host if request.client else None
+    user_agent = request.headers.get("User-Agent", "Unknown")
+    
+    log_data = {
+        "secret_id": secret.id,
+        "action": "read",
+        "ip_address": client_ip,
+        "user_agent": user_agent,
+        "ttl_seconds": secret.ttl_seconds,
+        "timestamp": datetime.now()
+    }
+    await db.execute(insert(SecretLog).values(**log_data))
+
+    await db.delete(secret)
+
+    await db.commit()
+
+    return {
+        "secret": secret
     }
--- app/schemas.py
+++ app/schemas.py
@@ -3,5 +3,6 @@ from datetime import date, datetime
 
 class CreateSecret(BaseModel):
     secret: str
-    passphrase: str | None = None  # Опционально, как в ТЗ
-    ttl_seconds: int | None = None  # Опционально, как в ТЗ
+    passphrase: str | None = None
+    ttl_seconds: int | None = None
+    secret_key: str
