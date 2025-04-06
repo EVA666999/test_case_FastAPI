dfe152896c3a923cc2b73e3e124f2638ff54d160
EVA666999 <ukratitelkisok9913@inbox.ru>
2025-04-06 08:23:40 +0300

    dasda

--- app/services/celery_service.py
+++ app/services/celery_service.py
@@ -16,13 +16,21 @@ def cleanup_expired_secrets():
         expired_secrets = db.execute(
             select(Secret).where(Secret.expires_at < now)
         ).scalars().all()
+        
         for secret in expired_secrets:
-            db.add(SecretLog(
+            log_entry = SecretLog(
                 secret_id=secret.id,
                 action="auto_delete",
-                additional_info="TTL expired"
-            ))
+                ip_address=None,
+                user_agent=None,
+                ttl_seconds=secret.ttl_seconds,
+                timestamp=now,
+                additional_info="Secret expired and automatically deleted"
+            )
+            db.add(log_entry)
+            
             db.delete(secret)
+        
         db.commit()
     
     return f"Удалено {len(expired_secrets)} просроченных секретов"
